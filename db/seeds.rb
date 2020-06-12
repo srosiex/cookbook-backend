7eba0b4657be5741856af6deacf69e6ff4b19d82
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-06-12 10:24:39 -0500

    adding seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Recipe.create(title: "Pasta", ingredients: "noodles, sauce, veggies", cuisine: "italian", favorite: false, directions: "boil noodles, add sauce, top with veggies", img: "https://www.wholesomelicious.com/wp-content/uploads/2015/05/Summer-Garden-Vegetable-Pasta.jpg")
-# Recipe.create(title: "Nachos", ingredients: "chips, cheez sauce, black beans, cilantro, quinoa blend, jalapenos", cuisine: "tex-mex", favorite: true, directions: "top chips with cheez and other toppings", img: "https://sidedish.dmagazine.com/wp-content/uploads/2015/03/IMG_17471.jpg")
+Recipe.create(title: "Pasta", ingredients: "noodles, sauce, veggies", cuisine: "italian", favorite: false, directions: "boil noodles, add sauce, top with veggies", img: "https://www.wholesomelicious.com/wp-content/uploads/2015/05/Summer-Garden-Vegetable-Pasta.jpg")
+Recipe.create(title: "Nachos", ingredients: "chips, cheez sauce, black beans, cilantro, quinoa blend, jalapenos", cuisine: "tex-mex", favorite: true, directions: "top chips with cheez and other toppings", img: "https://sidedish.dmagazine.com/wp-content/uploads/2015/03/IMG_17471.jpg")
 
 
-Comment.create(content: "Pretty good", recipe_id: 3)
-Comment.create(content: "Yummm", recipe_id: 4)
+# Comment.create(content: "Pretty good", recipe_id: 3)
+# Comment.create(content: "Yummm", recipe_id: 4)
